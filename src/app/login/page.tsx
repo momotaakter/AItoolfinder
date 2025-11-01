f38e37f0656b65a68eb71bb9a15f24bcb2775def
momota akter <momotaa808@gmail.com>
2025-11-01 16:41:15 +0600

    edit color in sign in page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -70,7 +70,7 @@ function LoginPage() {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#007A55] hover:bg-[#007A55] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Sign in
             </button>
